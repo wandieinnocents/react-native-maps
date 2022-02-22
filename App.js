abe4e646ae79302d85545260344f33019995cb46
wandieinnocents <wandieinnocent2@gmail.com>
2022-02-21 16:53:13 -0800

    Add long and lat

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView from "react-native-maps";
+import {useState} from "react";
 export default function App() {
+
+  const [region, setRegion] = useState({
+    latitude: 51.5079145,
+    longitude: -0.0899163,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+
   return (
-    <View style={styles.container}>
-    {/*Render our MapView*/}
-      <MapView
-        style={styles.map}
-        //specify our coordinates.
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    </View>
+   <View style={styles.container}>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      //onRegionChangeComplete runs when the user stops dragging MapView
+      onRegionChangeComplete={(region) => setRegion(region)}
+    />
+    {/*Display user's current region:*/}
+    <Text style={styles.text}>Current latitude: {region.latitude}</Text>
+    <Text style={styles.text}>Current longitude: {region.longitude}</Text>
+  </View>
   );
 }
 //create our styling code:
